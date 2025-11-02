99a7cd7bfa17541907871f7e230dfd1dd81a443b
xianting <swfdgryx@163.com>
2025-11-02 22:20:48 +0800

    first answer

--- 3n+1.c
+++ 3n+1.c
@@ -2,6 +2,20 @@
 
 int main()
 {
-	int n;
+	int n,i = 0;
 	scanf("%d",&n);
+	do{
+	if(n%2 == 1)
+	{
+		n = 3 * n + 1;
+		i++;
+	}
+	if(n%2 == 0 )
+	{
+		n /=2;
+		i++;
+	}
+	}
+	while(n!=1)
+	printf("%d",n);
 }
